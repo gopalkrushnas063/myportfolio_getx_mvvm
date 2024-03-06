920c6a1d719bd161a53942a7ac5c6b6f09260dd9
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2024-03-06 13:35:09 +0530

    project techstack updated

--- lib/view/project_view.dart
+++ lib/view/project_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:myportfolio_getx_mvvm/model/project_model.dart';
 import 'package:myportfolio_getx_mvvm/viewmodel/project_view_viewmodel.dart';
 
@@ -25,8 +26,8 @@ class ProjectView extends StatelessWidget {
                   ProjectModel project = projectController.projects[index];
                   return Card(
                     elevation: 3,
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 15),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -34,10 +35,10 @@ class ProjectView extends StatelessWidget {
                           padding: const EdgeInsets.all(5.0),
                           child: ClipRRect(
                             borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8),
-                              topRight: Radius.circular(8),
-                              bottomLeft: Radius.circular(8),
-                              bottomRight: Radius.circular(8),
+                              topLeft: Radius.circular(12),
+                              topRight: Radius.circular(12),
+                              bottomLeft: Radius.circular(12),
+                              bottomRight: Radius.circular(12),
                             ),
                             child: Image.network(
                               project.image,
@@ -65,6 +66,79 @@ class ProjectView extends StatelessWidget {
                                 project.description,
                                 style: const TextStyle(fontSize: 14),
                                 textAlign: TextAlign.justify,
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: project.technologies.map((tech) {
+                                  Widget imageWidget;
+                                  switch (tech.toLowerCase()) {
+                                    case 'flutter':
+                                      imageWidget = Image.network(
+                                        'https://gopalkrushnas063.github.io/images/skills/flutter.png',
+                                        height: 50,
+                                        width: 50,
+                                      );
+                                      break;
+                                    case 'dart':
+                                      imageWidget = Image.network(
+                                        'https://gopalkrushnas063.github.io/images/skills/dart.png',
+                                        height: 50,
+                                        width: 50,
+                                      );
+                                      break;
+                                    case 'html':
+                                      imageWidget = Image.network(
+                                        'https://gopalkrushnas063.github.io/images/skills/html.png',
+                                        height: 50,
+                                        width: 50,
+                                      );
+                                      break;
+                                    case 'css':
+                                      imageWidget = Image.network(
+                                        'https://gopalkrushnas063.github.io/images/skills/css.png',
+                                        height: 50,
+                                        width: 50,
+                                      );
+                                      break;
+                                    case 'android':
+                                      imageWidget = Image.network(
+                                        'https://gopalkrushnas063.github.io/images/skills/android.png',
+                                        height: 50,
+                                        width: 50,
+                                      );
+                                      break;
+                                    case 'github':
+                                      imageWidget = ColorFiltered(
+                                        colorFilter: const ColorFilter.mode(
+                                          Colors.black,
+                                          BlendMode.srcIn,
+                                        ),
+                                        child: Image.network(
+                                          'https://gopalkrushnas063.github.io/images/gitHub.png',
+                                          height: 50,
+                                          width: 50,
+                                        ),
+                                      );
+                                      break;
+                                    default:
+                                      imageWidget = Image.network(
+                                        'https://gopalkrushnas063.github.io/images/skills/java.png',
+                                        height: 50,
+                                        width: 50,
+                                      );
+                                  }
+
+                                  return Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        imageWidget,
+                                      ],
+                                    ),
+                                  );
+                                }).toList(),
                               ),
                             ],
                           ),
